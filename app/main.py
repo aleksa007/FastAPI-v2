0e0c9a271852b2560943f75fe60d14d9af0d6485
aleksa007 <ajelic007@gmail.com>
2022-11-06 16:09:01 +0100

    Mistake fixed from procfile

--- app/main.py
+++ app/main.py
@@ -3,18 +3,26 @@ from . import models
 from .db import engine
 from .routers import posts, users, auth, vote
 from .config import Settings
+from fastapi.middleware.cors import CORSMiddleware
 
-
-models.Base.metadata.create_all(bind=engine) #enigne from db
+#models.Base.metadata.create_all(bind=engine) #enigne from db
 
 app = FastAPI()  #instance" of the class FastAPI 
 
-"""
+
 @app.get("/")  #This decorator tells FastAPI that the function below corresponds to the path / with an operation get
 def root(): #Function that receives a request to the URL "/" using a GET operation
-    return {"message": "Hello Aleksa"}S
-"""
+    return {"message": "Hello Aleksa"}
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(posts.router)
